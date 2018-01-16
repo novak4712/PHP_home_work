778448887716f2f80207324cbc3c357824bbe598
Sorokin Sergey <novak4803@gmail.com>
2018-01-16 12:18:50 +0300

    work_home_PHP

---
+++ lessons/homework_4/registration.php
@@ -0,0 +1,45 @@
+<?php
+$error = []; //пустой массив для ошибок
+
+if (isset($_POST["submit"])) {
+    if (empty($_POST["email"])) {  //  проверил ввели ли адрес
+        $error [] = "введите ваш email адрес";
+    } else {
+        $email = $_POST["email"];
+    }
+
+    if ($_POST["login"]) {
+        if (mb_strlen($_POST["login"]) < 3) {      //  проверил логин на длинну
+            $error [] = "логин должен быть больше 3 символов";
+        } else if (mb_strlen($_POST["login"]) > 30) {   //  проверил логин на длинну
+            $error [] = "логин должен быть меньше 30 символов";
+        } else {
+            $login = $_POST["login"];
+        }
+    }else {
+            $error [] = "введите ваш логин";
+    }
+
+
+    if (empty($_POST["password"])) { //  проверил наличие пороля
+        $error [] = "введите ваш пароль";
+    } else {
+        $password = $_POST["password"];
+    }
+
+    if(count($error) == 0){        //  проверил наличие ошибок если их нет то записал в файл
+        $file = fopen("users.txt", "a+");
+        $user =
+            "Email: " . $email . " || " .
+            "Login: " . $login . " || " .
+            "Password: " . $password . PHP_EOL;
+        fwrite($file, $user);
+        fclose($file);
+    }
+    echo '<p style="color:red">';   //  если есть ошибки вывел на экран
+    foreach ($error as $value) {
+        echo $value."<br />";
+    }
+    echo '</p>';
+}
+?>
